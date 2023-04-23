09d65c709a3491da63460de39b83e8593c31719d
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-04-23 19:18:25 -0400

    added to html and css and js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 // I want to take a timed quiz on JS fundamentals that stores high scores
 
-// Step 1 - When I click the start button to begin taking the code quiz, a time will start.
+// Step 1 - When I click the start button to begin taking the code quiz, a timer will start.
 // Step 2 - When the timer starts, then I am presented with a question.
 // Step 3 - When I answer a question, then I am presented with another question.
 // Step 4 - If a question is answered incorrectly, then time is subtracted from the clock.
@@ -11,13 +11,28 @@ var quizContainer = document.getElementById('quiz');
 
 var quizQuestions = [
     {
-        question: "Arrays in JavaScript can be uses to store ______.",
-        answers: {
-            a: "Numbers and Strings",
-            b: "Other Arrays",
-            c: "Booleans",
-            d: "All of the above"
-        },
-        correctAnswer: "d"
+        question0: "Arrays in JavaScript can be uses to store ______.",
+        choices0: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+        answer0: "All of the above"
+    },
+    {
+        question1: "Commonly used data types do NOT include:",
+        choices1: ["Strings","Booleans", "Alerts", "Numbers",],
+        answer1: "Booleans"
+    },
+    {
+        question2: "The condition in an if/else statement is enclosed within ______.",
+        choices2: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+        answer2: "Parentheses"
+    },
+    {
+        question3: "String values must be enclosed within ______ when being assigned to variables.",
+        choices3: ["Commans", "Curly Brackets", "Quotes", "Parentheses"],
+        answer3: "Parentheses"
+    },
+    {
+        question4: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices4: ["JavaScript", "Terminal/Bash", "for loops", "console.log"],
+        answer4: "Parenthesis"
     }
 ];
