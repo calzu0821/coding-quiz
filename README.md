3c3d730e91fd07d31ed8c6ec5bbe081023b5cc7b
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-04-27 12:24:16 -0400

    adding README.md to quiz

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # coding-quiz
+
+## Description
+ 
+ This project entails the creation of a browser-based coding quiz that comprises multiple-choice questions centering around the fundamentals of JavaScript. The interface will be sleek and adaptable, featuring HTML and CSS updates that are powered by JavaScript.
+
+The motivation behind this project is to gain familiarity with coding assessments that are often a part of the interview process. By developing this app, I can apply the skills taught in this module and evaluate my progress.
+
+The app addresses the issue of assisting users in preparing for coding assessments by simulating a timed coding quiz with multiple-choice questions. Furthermore, it saves high scores, allowing users to track their advancement over time.
+
+This project has equipped me with the knowledge of manipulating HTML and CSS through JavaScript. Additionally, I have gained experience in managing events, creating functions, and dynamically updating web pages. I have also gained expertise in storing and retrieving data from local storage and displaying high scores on the app.
+
+## Installation
+
+To install this project, follow these steps:
+
+1. Clone the repository on your local machine using the following command:
+
+git clone https://github.com/calzu0821/coding-quiz.git
+
+2. Navigate to the repository's directory using the command:
+
+ cd coding-quiz
+
+3. Open the index.html file in your web browser to view the webpage.
+
+## Usage
+
+To use the Coding Quiz, follow these steps:
+
+1. Click the "Start Quiz" button to begin the timed quiz.
+2. Read each multiple-choice question and select an answer.
+3. If you select the correct answer, you will move on to the next question. If you select the incorrect answer, time will be subtracted from the clock.
+4. After you have answered all questions or the timer runs out, the game will be over.
+5. Enter your initials to save your score.
+6. View high scores by clicking the "View High Scores" button.
+
+Deployed website:
+
+
+
+Screenshot of the coding quiz:
+
+![alt text](../assets/images) ![alt text](../assets/images) ![alt text](../assets/images) 
+
+## Credits
+
+I used the following third-party assets in this project:
+
+- https://youtu.be/v7rSSy8CaYE
+- https://youtu.be/y17RuWkWdn8
+- https://youtu.be/8QLXD-8wbrc
+- https://youtu.be/YiOlaiscqDY
+- https://youtu.be/e57ReoUn6kM
+- https://youtu.be/0tEW8rB1bbU
+- https://youtu.be/JRevaOwNKTI
+- https://youtu.be/md8jQkOEyyk
+- https://youtu.be/UWCvbwo9IRk
+- https://youtu.be/Oive66jrwBs
+- https://www.flaticon.com/search?word=javascript
+- https://colorhunt.co/
+- https://www.youtube.com/watch?v=AUOzvFzdIk4
+- https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+- https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+- https://developer.mozilla.org/en-US/docs/Web/API/Storage
+- https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+- https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+- https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+
+## License
+
+N/A
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,38 +1,304 @@
-// I want to take a timed quiz on JS fundamentals that stores high scores
+//Creating a new variable name from the element ID on the HTML
+const startBtn = document.getElementById("start-btn");
+const startPage = document.getElementById("start-screen");
+const questionPage = document.getElementById("questions");
+const questionBtns = document.getElementsByClassName("question-button");
+const endPage = document.getElementById("end-screen");
+const highscorelink = document.getElementById("highscores-button");
+const timerDiv = document.getElementById("timer");
+const timerbtn = document.getElementById("timer1");
+const correctAnswer = document.getElementById("correct");
+const incorrectAnswer = document.getElementById("incorrect");
 
-// Step 1 - When I click the start button to begin taking the code quiz, a timer will start.
-// Step 2 - When the timer starts, then I am presented with a question.
-// Step 3 - When I answer a question, then I am presented with another question.
-// Step 4 - If a question is answered incorrectly, then time is subtracted from the clock.
-// Step 5 - When ALL the questions are answered OR the timer reaches 0, the game is over.
-// Step 6 - When the game is over, then I can save my initials and score.
-
-var quizContainer = document.getElementById('quiz');
-
-var quizQuestions = [
+//Added questions using an objects array
+const questions = [
     {
-        question0: "Arrays in JavaScript can be uses to store ______.",
-        choices0: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
-        answer0: "All of the above"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
     },
     {
-        question1: "Commonly used data types do NOT include:",
-        choices1: ["Strings","Booleans", "Alerts", "Numbers",],
-        answer1: "Booleans"
+        title: "The condition in an if/else statement is enclosed within ______.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
     },
     {
-        question2: "The condition in an if/else statement is enclosed within ______.",
-        choices2: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
-        answer2: "Parentheses"
+        title: "Arrays in JavaScript can be used to store _______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
     },
     {
-        question3: "String values must be enclosed within ______ when being assigned to variables.",
-        choices3: ["Commans", "Curly Brackets", "Quotes", "Parentheses"],
-        answer3: "Parentheses"
+        title: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
     },
     {
-        question4: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices4: ["JavaScript", "Terminal/Bash", "for loops", "console.log"],
-        answer4: "Parenthesis"
-    }
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
 ];
+
+const answers = [];
+let userFinal = [{ initials: "", score: 0 }];
+const userHighscores = [];
+for (let i = 0; i < questions.length; i++) {
+    answers.push(questions[i].answer)
+}
+let seconds = 75;
+let questionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    startPage.setAttribute("class", "hide");
+    questionPage.removeAttribute("class");
+    questionPage.setAttribute("class", "index");
+    timerDiv.removeAttribute("style");
+
+    setInterval(function timer() {
+        timerbtn.innerHTML = "Time: " + seconds;
+        seconds--;
+        if (seconds === 0 || seconds < 0) {
+            endQuiz();
+        }
+    }, 1000);
+
+    renderQuestion(questionIndex)
+}
+
+function renderQuestion(num) {
+    const question = questions[num];
+    if (question) {
+      const questionTitle = document.getElementById("question-title");
+      const questionBtn0 = document.getElementById("question0");
+      const questionBtn1 = document.getElementById("question1");
+      const questionBtn2 = document.getElementById("question2");
+      const questionBtn3 = document.getElementById("question3");
+  
+      questionTitle.innerHTML = question.title;
+      questionBtn0.innerHTML = question.choices[0];
+      questionBtn1.innerHTML = question.choices[1];
+      questionBtn2.innerHTML = question.choices[2];
+      questionBtn3.innerHTML = question.choices[3];
+    }
+  }
+
+for (let i = 0; i < questionBtns.length; i++) {
+    const button = questionBtns[i];
+    button.addEventListener("click", () => {
+        correctAnswer.setAttribute("class", "hide");
+        incorrectAnswer.setAttribute("class", "hide");
+        let answer = button.innerHTML;
+        if (answers.includes(answer) === true) {
+            correctAnswer.removeAttribute("class");
+            hideFeedBack();
+
+            if (questionIndex < questions.length) {
+                questionIndex++
+                renderQuestion(questionIndex);
+            }
+            else if (questionIndex === questions.length) {
+                score += seconds;
+                endQuiz();
+            }
+        }   else {
+            incorrectAnswer.removeAttribute("class");
+            hideFeedBack();
+
+            if (questionIndex < questions.length) {
+                questionIndex++
+                seconds -= 10;
+                renderQuestion(questionIndex);
+            }
+            else if (questionIndex === questions.length) {
+                seconds -= 10;
+                score += seconds;
+                endQuiz();
+            }
+        }
+    });
+}
+
+function hideFeedBack(){
+    setTimeout(() => {
+       correctAnswer.setAttribute("class","hide");
+       incorrectAnswer.setAttribute("class","hide");
+    }, 800);
+}
+ 
+function endQuiz() {
+    let finalScore = document.getElementById("score");
+    finalScore.innerHTML = score;
+
+    questionPage.setAttribute("class", "hide");
+    endPage.removeAttribute("class");
+    endPage.setAttribute("class", "index");
+    timerbtn.setAttribute("class", "hide");
+    highscorelink.setAttribute("class", "hide");
+}
+
+const submitBtn = document.getElementById("submit-form");
+submitBtn.addEventListener("click", function () {
+  let initials = document.getElementById("initials").value;
+  let pastScores = JSON.parse(localStorage.getItem("highscores")) || [];
+  let highScore = { initials: initials, score: score };
+  pastScores.push(highScore);
+  localStorage.setItem("highscores", JSON.stringify(pastScores));
+  displayHighscores();
+});
+
+function submitClear() {
+    alert("All highscores have been cleared!");
+    localStorage.clear();
+    window.location.reload();
+}
+
+const goBackBtn = document.getElementById("go-back-btn");
+goBackBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+function displayHighscores() {
+    let pastScores = JSON.parse(localStorage.getItem("highscores")) || [];
+    let highscoreList = document.getElementById("highscore-list");
+    highscoreList.innerHTML = "";
+    for (let i = 0; i < pastScores.length; i++) {
+      let highscore = pastScores[i];
+      let li = document.createElement("li");
+      li.textContent = highscore.initials + " - " + highscore.score;
+      highscoreList.appendChild(li);
+    }
+}
+
+highscorelink.addEventListener("click", function () {
+  startPage.setAttribute("class", "hide");
+  questionPage.setAttribute("class", "hide");
+  endPage.removeAttribute("class");
+  highscorelink.setAttribute("class", "hide");
+  timerbtn.setAttribute("class", "hide")
+  displayHighscores();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // I want to take a timed quiz on JS fundamentals that stores high scores
+
+// // Step 1 - When I click the start button to begin taking the code quiz, a timer will start.
+// var startButton = document.getElementById("start-button");
+// var startScreen = document.getElementById("start-quiz");
+// var questionScreen = document.getElementById("quiz-questions")
+// var timer = document.getElementById("timer");
+// var timer2 = document.getElementById("timer2");
+
+// var secondsLeft = 60;
+// var questionList = 0;
+// var score = 0;
+
+// startButton.onclick = startQuiz;
+
+// // function startQuiz () {
+// //     startScreen.setAttribute("class", "hide");
+// //     questionScreen.removeAttribute("class");
+// //     questionScreen.setAttribute("class", "highscores-container");
+// //     timer.removeAttribute("style")
+
+// //     setInterval(function timer() {
+// //         timer2.textContent = "Time: " + secondsLeft;
+// //         secondsLeft--;
+// //         if (secondsLeft <= 0) {
+// //             endQuiz();
+// //         }
+// //     }, 1000);
+// // }
+// // startQuiz();
+
+// // Step 2 - When the timer starts, then I am presented with a question.
+// // Step 3 - When I answer a question, then I am presented with another question.
+// // Step 4 - If a question is answered incorrectly, then time is subtracted from the clock.
+// // Step 5 - When ALL the questions are answered OR the timer reaches 0, the game is over.
+// // Step 6 - When the game is over, then I can save my initials and score.
+
+// // var quizContainer = document.getElementById('quiz');
+// // var timer = document.getElementById("time-box");
+// // var secondsLeft=60;
+
+// var quizQuestions = [
+//     {
+//         question: "Arrays in JavaScript can be uses to store ______.",
+//         choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+//         answer: "All of the above"
+//     },
+//     {
+//         question: "Commonly used data types do NOT include:",
+//         choices: ["Strings","Booleans", "Alerts", "Numbers",],
+//         answer: "Booleans"
+//     },
+//     {
+//         question: "The condition in an if/else statement is enclosed within ______.",
+//         choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+//         answer: "Parentheses"
+//     },
+//     {
+//         question: "String values must be enclosed within ______ when being assigned to variables.",
+//         choices: ["Commans", "Curly Brackets", "Quotes", "Parentheses"],
+//         answer: "Parentheses"
+//     },
+//     {
+//         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+//         choices: ["JavaScript", "Terminal/Bash", "for loops", "console.log"],
+//         answer: "Parenthesis"
+//     }
+// ];
+
+
+
+// // function startQuiz() {
+// //     // timer=setInterval(clockTick, 1000);
+// //     timer.textContent=setInterval(clockTick, 1000);
+// //     console.log(secondsLeft);
+// // }
+
+// // function clockTick() {
+// //     secondsLeft--;
+// //     timer.textContent=secondsLeft
+// //     if (secondsLeft <= 0) {
+// //         console.log ("Times up!");
+// //     }
+// // }
+// // startQuiz();
+
+// // function getQuestion() {
+// //     var currentQuestion=quizQuestions[0];
+// //     var questionTitle=document.getElementById("question-title");
+// //     questionTitle.textContent=currentQuestion.title
+
+// // }
+
+// // document.getElementById("start-button").onclick = function() {
+// //     var quizContainer = document.getElementById('quiz-container');
+// //     quizContainer.setAttribute('class', 'hidden');
+// //     getQuestion();
+// // }
